54022d699ca4f0457b0723c520707489cd47e2b5
Nolwenn Cadic <nolwenn.cadic@sonarsource.com>
2022-08-30 08:48:36 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import static java.lang.String.format;
+
+public class Main {
+  public static int addition(int a, int b) {
+    return a + b;
+  }
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 8;
+    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
+  }
+}
