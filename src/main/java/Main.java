db1f258d4f652721a2b8acb32b7bd12d59104c7f
Nolwenn Cadic <nolwenn.cadic@sonarsource.com>
2022-11-08 15:21:29 +0100

    Init main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,9 @@ public class Main {
   }
   public static void main(String[] args) {
     String s = "unused String";
-    int a = 5;;;;;;;;;
-    ;;;;;;;;;
+    String s2 = "unused String";
+    int a = 5;
     int b = 8;
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
-    System.out.println(format("%d + %d = %d", a, b, addition(a, b)));
+    System.out.println("On main branch");
   }
 }
